d2f078fa1f78bbb758a4f58bfac46aec9a097d93
UserBlackBox <userblackbox@tutanota.com>
2021-08-22 16:34:54 -0400

    update event serializer to new model

--- core/api/serializers/course.py
+++ core/api/serializers/course.py
@@ -26,14 +26,11 @@ class CourseSerializer(serializers.ModelSerializer):
         ]
 
 
-class EventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Event
-        fields = [
-            'name',
-            'term',
-            'description',
-            'start_date',
-            'end_date',
-            'is_instructional'
-        ]
+class EventSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=128)
+    description = serializers.CharField(allow_blank=True, style={'base_template': 'textarea.html'})
+    start_date = serializers.DateTimeField()
+    end_date = serializers.DateTimeField()
+    is_instructional = serializers.BooleanField(default=False)
+    organization = serializers.SlugRelatedField(slug_field='name', queryset=models.Organization.objects.all(), required=False, allow_null=True, allow_empty=True)
+    tags = serializers.SlugRelatedField(slug_field='name', many=True, queryset=models.Tag.objects.all())
