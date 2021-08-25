3a07a83da1dd465509020eafde0a1be4df502f72
UserBlackBox <userblackbox@tutanota.com>
2021-08-25 16:36:06 -0400

    modify TimetableSerializer to include term and course serialization

--- core/api/serializers/timetable.py
+++ core/api/serializers/timetable.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from ... import models
+from .course import TermSerializer, CourseSerializer
 
 
 class TimetableSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
     owner = serializers.SlugRelatedField(slug_field='username', queryset=models.User.objects.all())
-    term = serializers.SlugRelatedField(slug_field='name', queryset=models.Term.objects.all())
-    courses = serializers.SlugRelatedField(slug_field='code', queryset=models.Course.objects.all())
+    term = TermSerializer()
+    courses = CourseSerializer(many=True)
