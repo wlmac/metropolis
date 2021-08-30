57318ba059b5bbea075ebb0fb6932f8181cefe9b
Daniel Ye <danielyeqt@gmail.com>
2021-08-30 17:03:31 -0400

    fix settings.py

--- metropolis/settings.py
+++ metropolis/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'Change me'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
