ae0b6ba5c7d7acff232674e0c8b33bfbcdb17420
Justin Lu <wabasabi359@gmail.com>
2021-09-06 19:42:30 -0400

    start pwa install prompt

---
+++ core/static/core/js/install.js
@@ -0,0 +1,38 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can install the PWA
+    showInstallPromotion();
+    // Optionally, send analytics event that PWA install promo was shown.
+    console.log(`'beforeinstallprompt' event was fired.`);
+});
+
+
+$('#install-popup-button').click(function() {
+    clickd();
+});
+
+async function clickd(){
+    // Hide the app provided install promotion
+    hideInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
+    console.log(`User response to the install prompt: ${outcome}`);
+    // We've used the prompt, and can't use it again, throw it away
+    deferredPrompt = null;
+}
+
+function showInstallPromotion(){
+    $('.install-popup').show();
+}
+
+function hideInstallPromotion(){
+    $('.install-popup').hide();
+}
