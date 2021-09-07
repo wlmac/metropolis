8106707ad4943289d43bd9980a261ac991389466
Paul Lee <contact@paullee.dev>
2021-09-06 23:04:08 -0400

    add a2hs banner functional

--- core/static/core/js/install.js
+++ core/static/core/js/install.js
@@ -1,38 +1,32 @@
 let deferredPrompt;
+let installPopupClass = '.install-popup'
 
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent the mini-infobar from appearing on mobile
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI notify the user they can install the PWA
-    showInstallPromotion();
-    // Optionally, send analytics event that PWA install promo was shown.
-    console.log(`'beforeinstallprompt' event was fired.`);
+function displayInstallPrompt(event) {
+    event.preventDefault();
+    deferredPrompt = event;
+    if (Cookies.get('hide_install_prompt') != '1') {
+        $(installPopupClass).show();
+    }
+}
+
+window.addEventListener('beforeinstallprompt', displayInstallPrompt);
+
+window.addEventListener('load', (event) => {
+    document.getElementById('install-popup-button').addEventListener('click', async (e) => {
+        $(installPopupClass).hide();
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        deferredPrompt = null;
+    });
 });
 
-
-$('#install-popup-button').click(function() {
-    clickd();
-});
-
-async function clickd(){
-    // Hide the app provided install promotion
-    hideInstallPromotion();
-    // Show the install prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    const { outcome } = await deferredPrompt.userChoice;
-    // Optionally, send analytics event with outcome of user choice
-    console.log(`User response to the install prompt: ${outcome}`);
-    // We've used the prompt, and can't use it again, throw it away
+window.addEventListener('appinstalled', () => {
+    $(installPopupClass).hide();
     deferredPrompt = null;
-}
+    window.removeEventListener('beforeinstallprompt', displayInstallPrompt);
+});
 
-function showInstallPromotion(){
-    $('.install-popup').show();
-}
-
-function hideInstallPromotion(){
-    $('.install-popup').hide();
+function dismissInstallPrompt() {
+    Cookies.set('hide_install_prompt', '1', {expires: 7})
+    $(installPopupClass).hide();
 }
