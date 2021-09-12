8ace90a16caa73ed79f1b6b1742400d64b48dbdb
hirundinidae <diana4lin@gmail.com>
2021-09-12 03:51:15 -0400

    Style edit page

--- core/views/user.py
+++ core/views/user.py
@@ -29,7 +29,7 @@ class Profile(DetailView, mixins.TitleMixin):
     
 class ProfileUpdate(UpdateView, mixins.TitleMixin):
     model = models.User
-    fields = ['bio', 'timezone']
+    fields = ['bio', 'timezone', 'first_name', 'last_name']
     template_name = 'core/profile/update.html'
     success_url = reverse_lazy('profile_redirect')
     title = 'Update Profile'
