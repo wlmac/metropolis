8f434e180cf4b939b88c90faa156e0444903d765
Ken Shibata <kenxshibata@gmail.com>
2021-10-01 23:03:57 -0400

    for events

--- core/api/serializers/course.py
+++ core/api/serializers/course.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from .tag import TagSerializer
 from ... import models
 
@@ -17,8 +18,9 @@ class CourseSerializer(serializers.ModelSerializer):
 
 class EventSerializer(serializers.ModelSerializer):
     organization = serializers.SlugRelatedField(slug_field='name', queryset=models.Organization.objects.all())
+    organization_id = serializers.PrimaryKeyRelatedField(queryset=models.Organization.objects.all())
     tags = TagSerializer(many=True)
-    
+
     class Meta:
         model = models.Event
         exclude = ['schedule_format', 'is_instructional']
