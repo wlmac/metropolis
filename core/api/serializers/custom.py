9ab27ccfff7f664375cb9c4a8e62e978fa0c9960
Ken Shibata <kenxshibata@gmail.com>
2021-10-02 00:49:37 -0400

    for core.models.organization.Organization

---
+++ core/api/serializers/custom.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+
+class PrimaryKeyAndSlugRelatedField(serializers.SlugRelatedField):
+    def __init__(self, **kwargs):
+        self.pk_field = kwargs.pop('pk_field', None)
+        super().__init__(**kwargs)
+
+    def to_representation(self, obj):
+        if self.pk_field is not None:
+            pk = self.pk_field.to_representation(obj.pk)
+        else:
+            pk = obj.pk
+        return {
+            'id': pk,
+            'slug': getattr(obj, self.slug_field),
+        }
--- core/api/serializers/organization.py
+++ core/api/serializers/organization.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
+
+from .custom import PrimaryKeyAndSlugRelatedField
 from .tag import TagSerializer
 from ... import models
 
 
 class OrganizationSerializer(serializers.ModelSerializer):
-    owner = serializers.SlugRelatedField(slug_field='username', queryset=models.User.objects.all())
-    supervisors = serializers.SlugRelatedField(slug_field='username', many=True, queryset=models.User.objects.all())
-    execs = serializers.SlugRelatedField(slug_field='username', many=True, queryset=models.User.objects.all())
+    owner = PrimaryKeyAndSlugRelatedField(slug_field='username', queryset=models.User.objects.all())
+    owner_id = serializers.PrimaryKeyRelatedField(queryset=models.User.objects.all())
+    supervisors = PrimaryKeyAndSlugRelatedField(slug_field='username', many=True, queryset=models.User.objects.all())
+    execs = PrimaryKeyAndSlugRelatedField(slug_field='username', many=True, queryset=models.User.objects.all())
 
     tags = TagSerializer(many=True)
 
