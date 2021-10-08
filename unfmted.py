0c6c7d1aac9adde4d273bddc70915df3b61da2f4
Ken Shibata <kenxshibata@gmail.com>
2021-10-08 17:55:29 -0400

    gha/lint: test

---
+++ unfmted.py
@@ -0,0 +1,8 @@
+# import json
+#
+# import core.models.user
+# from core.models import Announcement, AbstractUser
+#
+# _, _, _, _ = json, core.models.user.User, Announcement, AbstractUser
+#
+# print(           'yeet')
