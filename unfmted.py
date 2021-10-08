becf07fd61d34c30ea9160a5d145b71d16aff182
Ken Shibata <kenxshibata@gmail.com>
2021-10-08 18:20:01 -0400

    gha/lint: remove test file

--- unfmted.py
+++
@@ -1,8 +0,0 @@
-import json
-
-import core.models.user
-from core.models import Announcement, AbstractUser
-
-_, _, _, _ = json, core.models.user.User, Announcement, AbstractUser
-
-print(           'yeet')
