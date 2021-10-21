6af94a84adc75e9f1ceeb5286b3cc22fca1b74c4
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-10-21 11:29:26 -0400

    sort execs based on alphabet and resolve promises together

--- core/static/core/js/tv/clubs.js
+++ core/static/core/js/tv/clubs.js
@@ -12,29 +12,39 @@ function setSlide() {
     }
     $(".bio").text(club.bio);
     $(".club-execs").empty();
+    var execlist = [];
+    var promiselist = [];
     for (var exec of club.execs) {
-        var name;
-        new Promise((resolve, reject) => {
+        var fname;
+        var lname;
+        promiselist.push(new Promise((resolve, reject) => {
             $.getJSON(window.location.origin + "/api/user/" + exec.slug, function (data) {
-                name = data.first_name + " " + data.last_name;
+                fname = data.first_name;
+                lname = data.last_name;
                 resolve();
             });
         }).then(() => {
-            $(".club-execs").append(`<p>${name}</p>`);
-        });
+            execlist.push({ fname: fname, lname: lname });
+        }));
     }
-    $(".description").html(marked(club.extra_content));
-    var elem = $("#scrollable1")
-    var time = Math.min(60000, elem.prop("scrollHeight") * 16);
-    elem.scrollTop(0);
-    setTimeout(function () { elem.animate({ scrollTop: elem.prop("scrollHeight") }, time) }, 30000);
-    elem = $("#scrollable2")
-    time = Math.min(60000, elem.prop("scrollHeight") * 32);
-    elem.scrollTop(0);
-    setTimeout(function () { elem.animate({ scrollTop: elem.prop("scrollHeight") }, time) }, 30000);
-    $("#fade").delay(119000).fadeTo(250, 1);
-    i++;
-    i %= clubs.length;
+    Promise.all(promiselist).then(() => {
+        execlist.sort((a, b) => (a.fname > b.fname) ? 1 : (a.fname === b.fname) ? ((a.lname > b.lname) ? 1 : -1) : -1);
+        for (let k = 0; k < execlist.length; k++) {
+            $(".club-execs").append(`<p>${execlist[k].fname + " " + execlist[k].lname}</p>`);
+        }
+        $(".description").html(marked(club.extra_content));
+        var elem = $("#scrollable1")
+        var time = Math.min(60000, elem.prop("scrollHeight") * 16);
+        elem.scrollTop(0);
+        setTimeout(function () { elem.animate({ scrollTop: elem.prop("scrollHeight") }, time) }, 30000);
+        elem = $("#scrollable2")
+        time = Math.min(60000, elem.prop("scrollHeight") * 32);
+        elem.scrollTop(0);
+        setTimeout(function () { elem.animate({ scrollTop: elem.prop("scrollHeight") }, time) }, 30000);
+        $("#fade").delay(119000).fadeTo(250, 1);
+        i++;
+        i %= clubs.length;
+    })
 }
 
 function slides() {
