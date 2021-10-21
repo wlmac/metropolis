e51b7ac0c62c09b781045cca6a3212c880312d83
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-10-21 11:08:06 -0400

    fix club tv not working

--- core/static/core/js/tv/clubs.js
+++ core/static/core/js/tv/clubs.js
@@ -7,15 +7,15 @@ function setSlide() {
     $(".club-logo").children().attr("src", club.icon);
     $(".club-name").text(club.name);
     $(".tag-section").empty();
-    for(var tag of club.tags) {
+    for (var tag of club.tags) {
         $(".tag-section").append(`<p class="tag" style="background-color: ${tag.color};">${tag.name}</p>`);
     }
     $(".bio").text(club.bio);
     $(".club-execs").empty();
-    for(var exec of club.execs) {
+    for (var exec of club.execs) {
         var name;
         new Promise((resolve, reject) => {
-            $.getJSON(window.location.origin+"/api/user/"+exec.slug, function(data) {
+            $.getJSON(window.location.origin + "/api/user/" + exec.slug, function (data) {
                 name = data.first_name + " " + data.last_name;
                 resolve();
             });
@@ -25,22 +25,22 @@ function setSlide() {
     }
     $(".description").html(marked(club.extra_content));
     var elem = $("#scrollable1")
-    var time = Math.min(60000, elem.prop("scrollHeight")*16);
+    var time = Math.min(60000, elem.prop("scrollHeight") * 16);
     elem.scrollTop(0);
-    setTimeout(function() {elem.animate({scrollTop: elem.prop("scrollHeight")}, time)}, 30000);
+    setTimeout(function () { elem.animate({ scrollTop: elem.prop("scrollHeight") }, time) }, 30000);
     elem = $("#scrollable2")
-    time = Math.min(60000, elem.prop("scrollHeight")*32);
+    time = Math.min(60000, elem.prop("scrollHeight") * 32);
     elem.scrollTop(0);
-    setTimeout(function() {elem.animate({scrollTop: elem.prop("scrollHeight")}, time)}, 30000);
+    setTimeout(function () { elem.animate({ scrollTop: elem.prop("scrollHeight") }, time) }, 30000);
     $("#fade").delay(119000).fadeTo(250, 1);
     i++;
     i %= clubs.length;
 }
 
 function slides() {
-    $.getJSON(window.location.origin+"/api/organizations", function(data) {
+    $.getJSON(window.location.origin + "/api/organizations", function (data) {
         clubs = data;
+        setTimeout(setSlide, 100);
+        setInterval(setSlide, 120000);
     });
-    setTimeout(setSlide, 100);
-    setInterval(setSlide, 120000);
 }
