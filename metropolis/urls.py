7c003ff61f814332740e9d74f2d06e538f295e38
Ken Shibata <kenxshibata@gmail.com>
2021-11-04 23:05:52 -0400

    urls: only allow authorize, token, revoke-token endpoints for all

--- metropolis/urls.py
+++ metropolis/urls.py
@@ -13,14 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import oauth2_provider.views as oauth2_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+oauth2_endpoint_views = [
+    path("authorize/", oauth2_views.AuthorizationView.as_view(), name="authorize"),
+    path("token/", oauth2_views.TokenView.as_view(), name="token"),
+    path("revoke-token/", oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
+]
+
 urlpatterns = [
     path("", include("core.urls")),
-    path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
+    path(
+        "o/",
+        include(
+            (oauth2_endpoint_views, "oauth2_provider"), namespace="oauth2_provider"
+        ),
+    ),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("martor/", include("martor.urls")),
