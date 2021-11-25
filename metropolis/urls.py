af5f69424775d20a3f1bec48f07d3c3ecbadd3c0
Ken Shibata <kenxshibata@gmail.com>
2021-11-24 23:32:33 -0500

    revive oauth /o/ pages

--- metropolis/urls.py
+++ metropolis/urls.py
@@ -25,6 +25,23 @@ oauth2_endpoint_views = [
     path("revoke-token/", oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
 ]
 
+
+# OAuth2 Application Management endpoints
+oauth2_endpoint_views += [
+    path('applications/', oauth2_views.ApplicationList.as_view(), name="list"),
+    path('applications/register/', oauth2_views.ApplicationRegistration.as_view(), name="register"),
+    path('applications/<pk>/', oauth2_views.ApplicationDetail.as_view(), name="detail"),
+    path('applications/<pk>/delete/', oauth2_views.ApplicationDelete.as_view(), name="delete"),
+    path('applications/<pk>/update/', oauth2_views.ApplicationUpdate.as_view(), name="update"),
+]
+
+# OAuth2 Token Management endpoints
+oauth2_endpoint_views += [
+    path('authorized-tokens/', oauth2_views.AuthorizedTokensListView.as_view(), name="authorized-token-list"),
+    path('authorized-tokens/<pk>/delete/', oauth2_views.AuthorizedTokenDeleteView.as_view(),
+        name="authorized-token-delete"),
+]
+
 urlpatterns = [
     path("", include("core.urls")),
     path(
