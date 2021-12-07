814233b482546b89f23b67cbfd57e53177121908
Ken S <45807097+colourdelete@users.noreply.github.com>
2021-12-06 21:25:07 -0500

    Revert "models ann: make title and body non-blankable"

--- core/models/post.py
+++ core/models/post.py
@@ -18,8 +18,8 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     last_modified_date = models.DateTimeField(auto_now=True)
 
-    title = models.CharField(max_length=64)
-    body = models.TextField()
+    title = models.CharField(max_length=64, blank=True)
+    body = models.TextField(blank=True)
     tags = models.ManyToManyField(
         "Tag", blank=True, related_name="%(class)ss", related_query_name="%(class)s"
     )
