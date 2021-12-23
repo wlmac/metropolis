6dcd86a7f85efbe9dc09fef59e5cd53c87fabb41
Ken Shibata <kenxshibata@gmail.com>
2021-12-22 22:22:31 -0500

    allow br and remove debug-only code

--- core/templatetags/markdown_tags.py
+++ core/templatetags/markdown_tags.py
@@ -10,6 +10,7 @@ register = template.Library()
 cleaner = sanitizer.Cleaner(
     tags=[
         *sanitizer.ALLOWED_TAGS,
+        "br",
         "p",
         "img",
         *["h{}".format(i) for i in range(1, 7)],
@@ -24,6 +25,4 @@ cleaner = sanitizer.Cleaner(
 
 @register.filter
 def markdown(field_name):
-    print(markdownify(field_name))
-    print(cleaner.clean(markdownify(field_name)))
     return mark_safe(cleaner.clean(markdownify(field_name)))
