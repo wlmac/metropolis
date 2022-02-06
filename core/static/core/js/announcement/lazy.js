faa14645964a9fefb0291385933fe003916ba87f
Daniel Ye <danielyeqt@gmail.com>
2022-02-06 13:48:25 -0500

    Load multiple cards at the same time

--- core/static/core/js/announcement/lazy.js
+++ core/static/core/js/announcement/lazy.js
@@ -3,36 +3,47 @@
  * @description : lazy loading of annoucements
  */
 
+let loadIn = new Map();
+let pagesLoaded = 0;
+let loadedLast = false;
+let loading = false;
+let cardsElem;
+
+function loadBuffer(){
+    if(!loading){
+        loading = true;
+        while(!loadedLast && loadIn.has(pagesLoaded + 1)){
+            cardsElem.insertAdjacentHTML('beforeend', loadIn.get(pagesLoaded + 1));
+            const nexts = cardsElem.getElementsByClassName('has-next');
+            for (let i = nexts.length - 2; i >= 0; i--) {
+                nexts[i].remove();
+            }
+            if(nexts[nexts.length - 1].classList.contains("no-more")) loadedLast = true;
+            pagesLoaded += 1;
+        }
+        loading = false;
+    }
+}
+
 async function loadPage(page, feed) {
     const response = await fetch(
         `/announcements/cards?page=${page}&feed=${feed}`,
     );
-    const cards = response.text();
-    return cards;
+    return response.text();
 }
 
-async function insertPage(cardsElem, page, feed) {
+async function insertPage(page, feed) {
     console.log(`loading page ${page} for feed ${feed}`);
     const cards = await loadPage(page, feed);
-    cardsElem.insertAdjacentHTML('beforeend', cards);
-    const nexts = cardsElem.getElementsByClassName('has-next');
-    for (let i = nexts.length - 2; i >= 0; i--) {
-        nexts[i].remove();
-    }
-    if (nexts[nexts.length-1].classList.contains('no-more')) {
-        return false;
-    }
-    return true;
+    loadIn.set(page, cards);
+    loadBuffer();
 }
 
 function setup(feedSlug) {
-    let page = 1;
-    let hasNext = true;
-    const cardsElem = document.getElementById(`cards-${feedSlug}`);
-    return async () => {
-        if (hasNext) {
-            hasNext = await insertPage(cardsElem, page, feedSlug);
-            page += 1;
+    cardsElem = document.getElementById(`cards-${feedSlug}`);
+    return async (page) => {
+        if (!loadedLast) {
+            await insertPage(page, feedSlug);
         }
     }
 }
