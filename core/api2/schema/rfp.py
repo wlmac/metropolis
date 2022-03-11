30fefd1520215ac3f215e716046e80863265fb2b
Ken Shibata <kenxshibata@gmail.com>
2022-03-10 21:15:16 -0500

    rfp: rejection_reason, is_public, gql

---
+++ core/api2/schema/rfp.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from typing import Optional
+
+import graphene
+from django.db.models import Q
+from graphene_django import DjangoObjectType
+
+from ...models import RfP
+from .post import StatusType
+
+
+class RfPType(DjangoObjectType):
+    def resolve_status(self, info):
+        if self.status == "a":
+            return StatusType.APPROVED
+        elif self.status == "p":
+            return StatusType.PENDING_APPROVAL
+        elif self.status == "r":
+            return StatusType.REJECTED
+        else:
+            raise Exception("Invalid status")
+
+    class Meta:
+        model = RfP
+
+
+class Schema:
+    rfps = graphene.List(
+        RfPType,
+        status=graphene.Argument(StatusType),
+    )
+
+    def resolve_rfps(
+        self,
+        info,
+        status: Optional[StatusType] = None,
+    ):
+        q = Announcement.objects.filter(
+            **({"status": StatusType.db_status(status)} if status is not None else {}),
+        )
+        if status is not StatusType.APPROVED:
+            q = q.union(RfP.accessible_by(info.context.user))
+        return q.distinct().all()
--- core/models/rfp.py
+++ core/models/rfp.py
@@ -17,6 +17,7 @@ class RfP(models.Model):
     )
     created_date = models.DateTimeField(auto_now_add=True)
     last_modified_date = models.DateTimeField(auto_now=True)
+    is_public = models.BooleanField(default=False)
 
     supervisor = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -28,6 +29,12 @@ class RfP(models.Model):
     status = models.CharField(
         max_length=1, choices=announcement_status_choices, default="p"
     )
+    rejection_reason = models.CharField(
+        max_length=140,
+        blank=True,
+        verbose_name="reason for rejection",
+        help_text="Only fill this field in if you are rejecting this announcement.",
+    )
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
@@ -35,6 +42,16 @@ class RfP(models.Model):
 
     def __str__(self):
         return "RfP for {}".format(self.content_object)
+
+    @classmethod
+    def accessible_by(cls, user):
+        if user.is_superuser:
+            return cls.objects.all()
+        return cls.objects.filter(
+            models.Q(is_public=True, status="a")
+            | models.Q(author=user)
+            | models.Q(supervisor=user)
+        )
 
     class Meta:
         ordering = ["-created_date"]
