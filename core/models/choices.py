3349214a7fbebb2e47e5da08260f76f66496fcbc
Patrick Lin <apocalypsecalculator@gmail.com>
2022-08-29 18:39:17 -0400

    add 2026 as grad year option

--- core/models/choices.py
+++ core/models/choices.py
@@ -8,10 +8,10 @@ timezone_choices = [(i, i) for i in pytz.common_timezones]
 
 graduating_year_choices = [
     (None, "Does not apply"),
-    (2022, 2022),
     (2023, 2023),
     (2024, 2024),
     (2025, 2025),
+    (2026, 2026),
 ]
 
 announcement_status_choices = [
