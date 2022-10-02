75640fc41ff66cf1e57870a8de3c365b1d0af382
Nyiyui / Ken S <45807097+nyiyui@users.noreply.github.com>
2022-10-02 11:13:05 -0400

    revert

--- static/js/mapbox-gl.js
+++ static/js/mapbox-gl.js
@@ -1,6 +1,4 @@
 /* Mapbox GL JS is Copyright © 2020 Mapbox and subject to the Mapbox Terms of Service ((https://www.mapbox.com/legal/tos/). */
-// noinspection JSDeprecatedSymbols
-
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 typeof define === 'function' && define.amd ? define(factory) :
