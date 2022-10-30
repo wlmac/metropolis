ac53c1570e340fc33a6e5899346ef340b464b4fa
Ken Shibata <nyiyui@nyiyui.ca>
2022-10-30 19:05:59 -0400

    raffle: fix error

--- core/views/raffle.py
+++ core/views/raffle.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from django.views.generic.base import RedirectView
 
@@ -13,12 +14,12 @@ class RaffleRedirect(LoginRequiredMixin, RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         rid = self.request.GET['r']
-        raffle = models.Raffle.get_object_or_404(rid)
+        raffle = get_object_or_404(models.Raffle, pk=rid)
         now = timezone.now()
         if raffle.open_start > now:
-            raise Http404()
+            raise Http404('s')
         if raffle.open_end < now:
-            raise Http404()
+            raise Http404('e')
         code = self.request.GET['c']
         if code in raffle.codes_win:
             return raffle.page_win
