c63c97beef338155c613636c0ffe8d1757da2505
Ken Shibata <nyiyui@nyiyui.ca>
2022-11-18 01:19:11 -0500

    fix openid-configuration

--- metropolis/urls.py
+++ metropolis/urls.py
@@ -19,74 +19,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-oauth2_endpoint_views = [
-    path("authorize/", oauth2_views.AuthorizationView.as_view(), name="authorize"),
-    path("token/", oauth2_views.TokenView.as_view(), name="token"),
-    path("revoke-token/", oauth2_views.RevokeTokenView.as_view(), name="revoke-token"),
-]
-
-
-# OAuth2 Application Management endpoints
-oauth2_endpoint_views += [
-    path("applications/", oauth2_views.ApplicationList.as_view(), name="list"),
-    path(
-        "applications/register/",
-        oauth2_views.ApplicationRegistration.as_view(),
-        name="register",
-    ),
-    path("applications/<pk>/", oauth2_views.ApplicationDetail.as_view(), name="detail"),
-    path(
-        "applications/<pk>/delete/",
-        oauth2_views.ApplicationDelete.as_view(),
-        name="delete",
-    ),
-    path(
-        "applications/<pk>/update/",
-        oauth2_views.ApplicationUpdate.as_view(),
-        name="update",
-    ),
-]
-
-# OAuth2 Token Management endpoints
-oauth2_endpoint_views += [
-    path(
-        "authorized-tokens/",
-        oauth2_views.AuthorizedTokensListView.as_view(),
-        name="authorized-token-list",
-    ),
-    path(
-        "authorized-tokens/<pk>/delete/",
-        oauth2_views.AuthorizedTokenDeleteView.as_view(),
-        name="authorized-token-delete",
-    ),
-]
-
-# OIDC endpoints
-oidc_views = [
-    path(
-        r".well-known/openid-configuration/",
-        oauth2_views.ConnectDiscoveryInfoView.as_view(),
-        name="oidc-connect-discovery-info",
-    ),
-    path(r".well-known/jwks.json", oauth2_views.JwksInfoView.as_view(), name="jwks-info"),
-    path(r"userinfo/", oauth2_views.UserInfoView.as_view(), name="user-info"),
-]
 
 urlpatterns = [
     path("", include("core.urls")),
-    path(
-        "o/",
-        include(
-            (oauth2_endpoint_views, "oauth2_provider"), namespace="oauth2_provider"
-        ),
-    ),
+    path("", include('oauth2_provider.urls', namespace='oauth2_provider')),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("martor/", include("martor.urls")),
     path("select2/", include("django_select2.urls")),
     path("", include("pwa.urls")),
     path("/<path:url>", include("django.contrib.flatpages.urls")),
-    *oidc_views,
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
