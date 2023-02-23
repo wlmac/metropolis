669abead84fa69c999a9bcec254f5020d3ae26bf
nyiyui <+@nyiyui.ca>
2023-02-22 21:23:48 -0500

    markdown emoji: ignore invalid

--- core/markdown/emoji.py
+++ core/markdown/emoji.py
@@ -3,17 +3,26 @@ import markdown
 EMOJI_RE = r"(:[+\-\w]+:)"
 
 REWRITES = {
-    'tada': 'party popper'
+    'checkered flag': 'chequered flag',
+    'grey question': 'question mark',
+    'white check mark': 'check mark',
+    'question': 'question mark',
+    'tada': 'party popper',
 }
 
 
 class EmojiPattern(markdown.inlinepatterns.Pattern):
     def handleMatch(self, m):
-        name = self.unescape(m.group(2)).replace('_', ' ')[1:-1]
+        orig = self.unescape(m.group(2))
+        name = orig.replace('_', ' ')[1:-1]
         name = REWRITES.get(name, name)
         emoji = r'\N{' + name + r'}'
-        converted = bytes(emoji, 'utf-8').decode('unicode_escape')
-        return converted
+        try:
+            converted = bytes(emoji, 'utf-8').decode('unicode_escape')
+        except UnicodeDecodeError:
+            return orig
+        else:
+            return converted
 
 
 class EmojiExtension(markdown.Extension):
