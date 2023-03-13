7e107f292e42d85c4432ad7ddd4ab4e7c4fdf49e
nyiyui <+@nyiyui.ca>
2023-03-12 20:02:43 -0400

    fix some deprecation warnings

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -i bash ./default.nix
-
-set -eu
-python -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
-python -m poetry run pip freeze > requirements.txt
-./manage.py runserver
