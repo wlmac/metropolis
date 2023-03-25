861b96720410cfb4933e1d64a3c061896a5436fb
Jason <jasoncameron.all@gmail.com>
2023-03-24 21:27:35 -0400

    fmt

--- core/views/post.py
+++ core/views/post.py
@@ -262,7 +262,7 @@ class BlogPostDetail(UserPassesTestMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['blogpost'].increment_views()
+        context["blogpost"].increment_views()
         context["title"] = self.get_title()
         return context
 
