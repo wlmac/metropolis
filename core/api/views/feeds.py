dc47949812de570628f1e48c4507d4fe6fe58d4a
nyiyui <+@nyiyui.ca>
2023-03-26 17:34:14 -0400

    fix __all__

--- core/api/views/feeds.py
+++ core/api/views/feeds.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-__ALL__ = ["feeds"]
+__all__ = ["feeds"]
 
 
 @api_view()
--- core/api/views/staff.py
+++ core/api/views/staff.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-__ALL__ = ["staff"]
+__all__ = ["staff"]
 
 
 staff_roles = defaultdict(list)
