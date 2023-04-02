d01ca438e81b582ac20138831d650843c8ddf388
nyiyui <+@nyiyui.ca>
2023-04-02 06:13:44 -0400

    playwright: auth

--- tests/api3.spec.js
+++ tests/api3.spec.js
@@ -12,3 +12,20 @@ test('sanity 2', async ({ request }) => {
   await request.get('/api/v3/feeds');
   await request.get('/api/v3/banners');
 });
+
+test('token-based auth', async ({ request }) => {
+  const auth = await request.post('/api/auth/token', {
+    data: {
+      username: 'sotokanda',
+      password: 'verysecure',
+    },
+  });
+  const tokens = await auth.json;
+  const ctx = await request.newContext({
+    extraHTTPHeaders: {
+      "Authentication": `Bearer ${tokens.access}`,
+    },
+  });
+  // TODO: refresh the token
+  await ctx.get('/api/me');
+});
