89eb8c07ef914e56ab5b7a80d69f7a24716c6b5e
nyiyui <+@nyiyui.ca>
2023-04-10 00:46:22 -0400

    api3: event: simplify query start/end logic
    and hopefully fix a bug where
    https://maclyonsden.com/api/v3/obj/event?start=2023-03-22&end=2023-03-22
    would return events that start 2023-03-21T00:00:00 and end at
    2023-03-21T23:59:59

--- core/api/views/objects/event.py
+++ core/api/views/objects/event.py
@@ -1,8 +1,11 @@
+import datetime
+
 from django.contrib.admin.models import LogEntry
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
 from django.utils import timezone
 from rest_framework import permissions, serializers
+from rest_framework.exceptions import ParseError
 
 from .announcement import Inner
 from .base import BaseProvider
@@ -45,63 +48,33 @@ class EventProvider(BaseProvider):
         )
 
     def get_queryset(self, request):
-        start = timezone.now()
-        if self.request.data.get("start"):
-            start = self.request.data.get("start")
-        elif self.request.query_params.get("start"):
-            start = self.request.query_params.get("start")
+        q = models.Event.objects.all()
 
-        if not self.request.user.is_anonymous:
-            if self.request.data.get("end"):
-                end = self.request.data.get("end")
-                events = (
-                    models.Event.objects.filter(
-                        end_date__gte=start, start_date__lte=end
-                    )
-                    .filter(
-                        Q(is_public=True) | Q(organization__member=self.request.user.id)
-                    )
-                    .distinct()
-                    .order_by("start_date")
-                )
-            elif self.request.query_params.get("end"):
-                end = self.request.query_params.get("end")
-                events = (
-                    models.Event.objects.filter(
-                        end_date__gte=start, start_date__lte=end
-                    )
-                    .filter(
-                        Q(is_public=True) | Q(organization__member=self.request.user.id)
-                    )
-                    .distinct()
-                    .order_by("start_date")
-                )
-            else:
-                events = (
-                    models.Event.objects.filter(end_date__gte=start)
-                    .filter(
-                        Q(is_public=True) | Q(organization__member=self.request.user.id)
-                    )
-                    .distinct()
-                    .order_by("start_date")
-                )
+        def parse(name):
+            raw = self.request.query_params.get(name)
+            if not raw:
+                return None
+            try:
+                return datetime.datetime.strptime(raw, "%Y-%m-%d")
+            except ValueError as e:
+                raise ParseError(detail=f"parse {name}: {e}")
+
+        start, end = parse("start"), parse("end")
+        if start:
+            print("start", start)
+            q = q.filter(end_date__gte=start)
+        if end:
+            print("end", start)
+            q = q.filter(start_date__lte=end)
+        if start and end and start > end:
+            raise ParseError("start is after end")
+        if self.request.user.is_anonymous:
+            q = q.filter(is_public=True)
         else:
-            if self.request.data.get("end"):
-                end = self.request.data.get("end")
-                events = models.Event.objects.filter(
-                    end_date__gte=start, start_date__lte=end, is_public=True
-                ).order_by("start_date")
-            elif self.request.query_params.get("end"):
-                end = self.request.query_params.get("end")
-                events = models.Event.objects.filter(
-                    end_date__gte=start, start_date__lte=end, is_public=True
-                ).order_by("start_date")
-            else:
-                events = models.Event.objects.filter(
-                    end_date__gte=start, is_public=True
-                ).order_by("start_date")
-
-        return events
+            q = q.filter(
+                Q(is_public=True) | Q(organization__member=self.request.user.id)
+            )
+        return q
 
     def get_last_modified(self, view):
         return (
