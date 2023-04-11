a7ca2cdfa91d21499fe37c77cc705c15260a1f64
Jason <jasoncameron.all@gmail.com>
2023-04-10 21:40:46 -0400

    fixed a small import issue

--- core/models/util.py
+++
@@ -1,3 +0,0 @@
-daysPerMonth = {'January', 31, 'February', 28, 31, 'March', 'April', 30, 31, 'May', 'June', 30, 'July', 31, 31,
-                'August', 'September', 30, 'October', 31, 'November', 30, 'December', 31}
-
--- core/utils/utils.py
+++ core/utils/utils.py
@@ -5,3 +5,11 @@ def get_week_and_day(dt: datetime.date):
     week_num = (dt.day - 1) // 7 + 1
     day_num = dt.weekday()
     return week_num, day_num
+
+
+daysPerMonth = {'January', 31, 'February', 28, 31, 'March', 'April', 30, 31, 'May', 'June', 30, 'July', 31, 31,
+                'August', 'September', 30, 'October', 31, 'November', 30, 'December', 31}
+
+
+months = {1, 'January', 2, 'February', 3, 'March', 4, 'April', 5, 'May', 6, 'June', 7, 'July', 8, 'August', 9, 'September', 10, 'October', 11, 'November', 12, 'December'}
+
