0b547f05d5d80f1bbad9cf55a84cd550264c9d89
Jason <jasoncameron.all@gmail.com>
2023-04-10 22:23:44 -0400

    RRule modal:
    - switched r.RecurrenceOptions and r.DaySOfWeek to the dateutil options.
    - Added r.MonthsOfYear
    - renamed repeats_every to interval and switched to custom PositiveOneSmallIntegerField
    - added repeat_months for type=monthly or yearly
    
    - switched ends_after to PositiveOneSmallIntegerField
    - started work on the internal calculations needed for this
    
    switched constraints to the clean method for better admin feel.
    
    utils:
    - bypassed months by simply using the month number as the key
    + format

--- core/utils/utils.py
+++ core/utils/utils.py
@@ -7,9 +7,17 @@ def get_week_and_day(dt: datetime.date):
     return week_num, day_num
 
 
-daysPerMonth = {'January', 31, 'February', 28, 31, 'March', 'April', 30, 31, 'May', 'June', 30, 'July', 31, 31,
-                'August', 'September', 30, 'October', 31, 'November', 30, 'December', 31}
-
-
-months = {1, 'January', 2, 'February', 3, 'March', 4, 'April', 5, 'May', 6, 'June', 7, 'July', 8, 'August', 9, 'September', 10, 'October', 11, 'November', 12, 'December'}
-
+daysPerMonth = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31,
+}
