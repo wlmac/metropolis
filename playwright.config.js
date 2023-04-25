74bedd9e0e00a369929c0d8d79f378527ab0dc63
Jason <jasoncameron.all@gmail.com>
2023-04-24 23:10:21 -0400

    try to fix gh

--- playwright.config.js
+++ playwright.config.js
@@ -68,11 +68,11 @@ module.exports = defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'python3 manage.py runserver',
-  //   url: 'http://127.0.0.1:8000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  //Run your local dev server before starting the tests */
+  webServer: {
+    command: 'python3 manage.py runserver',
+    url: 'http://127.0.0.1:8000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
 
