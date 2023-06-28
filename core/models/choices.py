742923997828975371b15162ad4e1de8593bf149
nyiyui <+@nyiyui.ca>
2023-06-28 02:09:29 -0400

    update graduating year choice (-2023 and +2027)
    This disables account creation for 2023 people but I think no one is
    making a new account when exams are over so...
    
    Thank you Patrick for reminding me!

--- core/models/choices.py
+++ core/models/choices.py
@@ -4,10 +4,10 @@ timezone_choices = [(i, i) for i in pytz.common_timezones]
 
 graduating_year_choices = [
     (None, "Does not apply"),
-    (2023, 2023),
     (2024, 2024),
     (2025, 2025),
     (2026, 2026),
+    (2027, 2027),
 ]
 
 announcement_status_choices = [
