28be5bed694f6de6814a1b39d36bc16f170b5ab1
Jason <jasoncameron.all@gmail.com>
2023-09-23 17:07:27 -0400

    feat (organizations): added small script to clean up the groups

--- core/tasks.py
+++ core/tasks.py
@@ -48,24 +48,45 @@ def users_with_token():
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(crontab(hour=18, minute=0), notif_events_singleday)
-    sender.add_periodic_task(crontab(day_of_week=0), clear_unused_users)
+    sender.add_periodic_task(crontab(day_of_month=0), clear_unused_owners)
 
 
 @app.task
-def clear_unused_users():
+def clear_unused_owners():
+    # owner_group, _ = Group.objects.get_or_create(name="Org Owners")
+    # for user in owner_group.user_set.all()():
+    #    if user.organizations_owning.count() == 0:
+    #        user.groups.remove(owner_group)
+
+    # will remove after one run-through on the server
     owner_group, _ = Group.objects.get_or_create(name="Org Owners")
     execs_group, _ = Group.objects.get_or_create(name="Execs")
-    all_staff = execs_group.user_set.all() | owner_group.user_set.all()
-    for user in all_staff.distinct():
-        print(
-            f"Checking {user} - {user.organizations_leading.count()} - {user.organizations_owning.count()}"
-        )
-        if user.organizations_leading.count() == 0:
-            user.groups.remove(execs_group)
-            print(f"Removed {user} from execs group")
-        if user.organizations_owning.count() == 0:
-            user.groups.remove(owner_group)
-            print(f"Removed {user} from owners group")
+    count_exec = {"added": 0, "removed": 0}
+    count_leading = {"added": 0, "removed": 0}
+    for user in User.objects.all():
+        if user.organizations_leading.count() >= 1:
+            if not user.groups.filter(name="Execs").exists():
+                user.groups.add(execs_group)
+                count_exec["added"] += 1
+        else:
+            if user.groups.filter(name="Execs").exists():
+                user.groups.remove(execs_group)
+                count_exec["removed"] += 1
+        if user.organizations_owning.count() >= 1:
+            if not user.groups.filter(name="Org Owners").exists():
+                user.groups.add(owner_group)
+                count_leading["added"] += 1
+        else:
+            if user.groups.filter(name="Org Owners").exists():
+                user.groups.remove(owner_group)
+                count_leading["removed"] += 1
+    print(f"Added {count_exec['added']} users to execs group")
+    print(f"Removed {count_exec['removed']} users from execs group")
+    print(f"Added {count_leading['added']} users to owners group")
+    print(f"Removed {count_leading['removed']} users from owners group")
+
+    print("total in execs group: " + str(execs_group.user_set.count()))
+    print("total in owners group: " + str(owner_group.user_set.count()) + "\n")
 
 
 @app.task
@@ -188,3 +209,6 @@ def notif_single(self, recipient_id: int, msg_kwargs):
         for token in notreg_tokens:
             del u.expo_notif_tokens[token]
         u.save()
+
+
+clear_unused_owners()
