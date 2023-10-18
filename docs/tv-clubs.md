df3f74335ef4c86a24c84afc84f066def7b78b49
nyiyui <+@nyiyui.ca>
2023-10-18 11:43:02 -0400

    add 2023-2024 schedule
    Make a new schedule as the late start this year is different.

--- docs/tv-clubs.md
+++ docs/tv-clubs.md
@@ -3,3 +3,5 @@
 Page used on the cafeteria projector during Club Crawl 2021, 2022 and 2023.
 
 `core/static/core/img/tv/data.hcl` has the layout of the tables, which can be edited to regenerate `.../gen-map.svg`.
+
+The clubs are mapped to each table on `core/static/core/js/tv/club-data.json`.
--- metropolis/settings.py
+++ metropolis/settings.py
@@ -690,6 +690,164 @@ TIMETABLE_FORMATS = {  # possible formats for timetables, position is the classe
             ],
         },
     },
+    "2023-2024": {
+        "day_num_method": "calendar_days",
+        "schedules": {
+            "default": [
+                {
+                    "description": {
+                        "time": "09:00 am - 10:20 am",
+                        "course": "Period 1",
+                    },
+                    "time": [[9, 0], [10, 20]],
+                    "position": [{1, 5}, {1, 5}],
+                },
+                {
+                    "description": {
+                        "time": "10:25 am - 11:40 am",
+                        "course": "Period 2",
+                    },
+                    "time": [[10, 25], [11, 40]],
+                    "position": [{2, 5}, {2, 5}],
+                },
+                {
+                    "description": {
+                        "time": "12:40 pm - 01:55 pm",
+                        "course": "Period 3",
+                    },
+                    "time": [[12, 40], [13, 55]],
+                    "position": [{3, 6}, {4, 6}],
+                },
+                {
+                    "description": {
+                        "time": "02:00 pm - 03:15 pm",
+                        "course": "Period 4",
+                    },
+                    "time": [[14, 0], [15, 15]],
+                    "position": [{4, 6}, {3, 6}],
+                },
+            ],
+            "late-start": [
+                {
+                    "description": {
+                        "time": "10:00 am - 10:55 am",
+                        "course": "Period 1",
+                    },
+                    "time": [[10, 0], [10, 55]],
+                    "position": [{1, 5}, {1, 5}],
+                },
+                {
+                    "description": {
+                        "time": "11:00 am - 12:00 pm",
+                        "course": "Period 2",
+                    },
+                    "time": [[11, 0], [12, 0]],
+                    "position": [{2, 5}, {2, 5}],
+                },
+                {
+                    "description": {
+                        "time": "01:05 pm - 02:10 pm",
+                        "course": "Period 3",
+                    },
+                    "time": [[13, 5], [14, 10]],
+                    "position": [{3, 6}, {4, 6}],
+                },
+                {
+                    "description": {
+                        "time": "02:15 pm - 03:15 pm",
+                        "course": "Period 4",
+                    },
+                    "time": [[14, 15], [15, 15]],
+                    "position": [{4, 6}, {3, 6}],
+                },
+            ],
+            "half-day": [
+                {
+                    "description": {
+                        "time": "09:00 am - 09:45 am",
+                        "course": "Period 1",
+                    },
+                    "time": [[9, 0], [9, 45]],
+                    "position": [{1, 5}, {1, 5}],
+                },
+                {
+                    "description": {
+                        "time": "09:50 am - 10:35 pm",
+                        "course": "Period 2",
+                    },
+                    "time": [[9, 50], [10, 35]],
+                    "position": [{2, 5}, {2, 5}],
+                },
+                {
+                    "description": {
+                        "time": "10:40 pm - 11:25 pm",
+                        "course": "Period 3",
+                    },
+                    "time": [[10, 40], [11, 25]],
+                    "position": [{3, 6}, {4, 6}],
+                },
+                {
+                    "description": {
+                        "time": "11:30 pm - 12:15 pm",
+                        "course": "Period 4",
+                    },
+                    "time": [[11, 30], [12, 15]],
+                    "position": [{4, 6}, {3, 6}],
+                },
+            ],
+            "sac-election": [
+                {
+                    "description": {
+                        "time": "09:00 am - 10:40 am",
+                        "course": "Period 1",
+                    },
+                    "time": [[9, 0], [10, 40]],
+                    "position": [{1, 5}, {1, 5}],
+                },
+                {
+                    "description": {
+                        "time": "10:45 am - 12:10 pm",
+                        "course": "Period 2",
+                    },
+                    "time": [[10, 45], [12, 10]],
+                    "position": [{2, 5}, {2, 5}],
+                },
+                {
+                    "description": {
+                        "time": "01:10 pm - 02:10 pm",
+                        "course": "Period 3",
+                    },
+                    "time": [[13, 10], [14, 10]],
+                    "position": [{3, 6}, {4, 6}],
+                },
+                {
+                    "description": {
+                        "time": "02:15 pm - 03:15 pm",
+                        "course": "Period 4",
+                    },
+                    "time": [[14, 15], [15, 15]],
+                    "position": [{4, 6}, {3, 6}],
+                },
+            ],
+        },
+        "courses": 4,
+        "positions": {1, 2, 3, 4, 5, 6},
+        "cycle": {
+            "length": 2,
+            "duration": "day",
+        },
+        "question": {
+            "prompt": "On Day 1, which period is this course in?",
+            "choices": [
+                (1, "Period 1"),
+                (2, "Period 2"),
+                (3, "Period 3"),
+                (4, "Period 4"),
+                (5, "This course is a 2-credit Co-op in the morning."),
+                (6, "This course is a 2-credit Co-op in the afternoon."),
+            ],
+        },
+    },
 }
 
 # Authentication settings
