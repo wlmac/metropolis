daa0c61286f0a6e02bf4b8ebcef4ac9731df61c7
Jason <jasoncameron.all@gmail.com>
2023-12-02 22:54:54 -0500

    Updated Pagination max to 30 due to changes in formatting.

--- metropolis/settings.py
+++ metropolis/settings.py
@@ -223,7 +223,7 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 50,
+    "PAGE_SIZE": 30,
 }
 
 # SSO (OAuth) Settings
