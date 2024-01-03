29baccb87e844396af82d26a6538b3bad3df6285
Jason Cameron <git@jasoncameron.dev>
2024-01-02 22:24:48 -0500

    feat: optimized table checking.

--- core/api/utils/github.py
+++ core/api/utils/github.py
@@ -1,62 +1,51 @@
 from functools import lru_cache
 from os import environ
-from typing import Iterable, Tuple, Optional, Dict, List
+from typing import Iterable, Tuple, Optional, List, TYPE_CHECKING
 
-from django.apps import apps
 from django.db import connection
 from django.db.models import Model
+from frozendict import frozendict
+
+if TYPE_CHECKING:
+	from frozendict import SelfT
 
 
 def table_exists(model: Model) -> bool:
 	"""
 	Check if the database table corresponding to the model exists.
 	"""
-	app_label = model._meta.app_label
-	model_name = model.__name__
-	
-	# Check if the model is registered with Django
-	if apps.get_model(app_label, model_name) is None:
-		return False
-	
 	# Check if the corresponding database table exists
 	table_name = model._meta.db_table
 	return table_name in connection.introspection.table_names()
 
 
-def get_model_choices(
-		model: Model,
-		value_list: Tuple[str, str],
-		db_filter: Optional[Dict[str, str]] = None,
-) -> List:
-	"""
-	Get choices for a model with optional filtering.
-	"""
-	if not table_exists(model):
-		return []
+def get_model_choices(model: Model, value_list: Tuple[str, str], db_filter: Optional["SelfT"] = None, ) -> List:
+	db_filter = frozendict(db_filter) if db_filter else None
 	
-	options: List
-	if db_filter is None:
-		options = list(model.objects.all().values_list(*value_list))
-	else:
-		options = list(model.objects.filter(**db_filter).values_list(*value_list))
+	@lru_cache(maxsize=None, typed=False)
+	def _get_model_choices(model, value_list, db_filter, ):
+		"""
+		Get choices for a model with optional filtering.
+		"""
+		if not table_exists(model):
+			return []
+		
+		options: List
+		if db_filter is None:
+			options = list(model.objects.all().values_list(*value_list))
+		else:
+			options = list(model.objects.filter(**db_filter).values_list(*value_list))
+			
+		return ci_safe(options)
 	
-	# Convert the list of tuples into a tuple of tuples before sorting
-	options = tuple(options)
-	filter_key = tuple(sorted(options, key=lambda x: x[0])) if db_filter else None
-	
-	return ci_safe(options, model=model, filter_key=filter_key)
+	return _get_model_choices(model, value_list, db_filter)
 
 
-@lru_cache(maxsize=None, typed=False)
-def ci_safe(
-		options: Iterable, /, model: Model, filter_key: Optional[Tuple] = None
-) -> Iterable:
+def ci_safe(options: Iterable) -> Iterable:
 	"""
 	Make options case-insensitive safe, returning an empty list in CI.
 	"""
-	if (
-			filter_key is not None and environ.get("GITHUB_ACTIONS", False) is True
-	):  # if running in CI
+	if environ.get("GITHUB_ACTIONS", False) is True:  # if running in CI
 		return []
 	else:
-		return options  # running in prod
+		return options
