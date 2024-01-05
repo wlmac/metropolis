e5fbed95ae4c9a7d6a10c7bf5485836364a2b60e
Jason Cameron <git@jasoncameron.dev>
2024-01-04 19:12:19 -0500

    I dont have nix installed so byeye lock

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
     flake-utils.url = github:numtide/flake-utils;
     poetry2nix.url = github:nix-community/poetry2nix;
     poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
