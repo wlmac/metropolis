52d0bd2e52a4635b1a38a1b32ecc672d50c3aa3b
Jason Cameron <git@jasoncameron.dev>
2024-02-06 21:28:32 -0500

    Some little touchups

--- core/management/commands/add_clubs.py
+++ core/management/commands/add_clubs.py
@@ -1,8 +1,6 @@
 import csv
-import numpy as np
 import requests
 
-from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import IntegrityError
 from core.models import User, Organization
@@ -13,42 +11,59 @@ class Command(BaseCommand):
     help = "Adds organizations from Google Sheets. Does not modify existing organizations. See https://github.com/wlmac/metropolis/issues/247"
 
     def add_arguments(self, parser):
-        parser.add_argument('sheets_link', type=str, help='Link to Google Sheets (must be published as CSV)')
+        parser.add_argument(
+            "sheets_link",
+            type=str,
+            help="Link to Google Sheets (must be published as CSV)",
+        )
 
     def handle(self, *args, **options):
-        assert "?output=csv" in options['sheets_link'], \
-            "Make sure make a copy of the club spreadsheet and use the link provided when publishing as CSV (https://support.google.com/docs/answer/183965)"
-        
-        csv_reader = csv.reader(
-                        StringIO(
-                            requests.get( options['sheets_link'] ).text
-                        )
-                    )
+        sheets_url = str(options["sheets_link"])
+        print(sheets_url)
+        if not ("output=csv" in sheets_url or sheets_url.endswith(".csv")):
+            print(sheets_url.endswith("?output=csv"))
+            raise AssertionError(
+                "Make sure make a copy of the club spreadsheet and use the link provided when publishing as CSV (https://support.google.com/docs/answer/183965)"
+            )
+
+        csv_reader = csv.reader(StringIO(requests.get(sheets_url).text))
 
         expected_header = [
-                    "CLUB NAME",
-                    "APPROVED & MAILED",
-                    "SAF PAID",
-                    "PRESIDENT(S)",
-                    "PRESIDENT(S) EMAIL",
-                    "STAFF SUPERVISOR(S)",
-                    "STAFF(S) EMAIL",
-                    "BUDGET REQUEST",
-                    "TIME + PLACE",
-                    "SOCIAL LINKS"
-                 ]
+            "CLUB NAME",
+            "APPROVED & MAILED",
+            "SAF PAID",
+            "PRESIDENT(S)",
+            "PRESIDENT(S) EMAIL",
+            "STAFF SUPERVISOR(S)",
+            "STAFF(S) EMAIL",
+            "BUDGET REQUEST",
+            "TIME + PLACE",
+            "SOCIAL LINKS",
+        ]
 
-        assert expected_header == next(csv_reader), \
-            "Google Sheets layout changed since the last time the script was updated"
+        assert expected_header == next(
+            csv_reader
+        ), "Google Sheets layout changed since the last time the script was updated"
 
         for row in csv_reader:
-            organization_is_not_approved = row[1]!='TRUE'
+            organization_is_not_approved = row[1] != "TRUE"
             has_duplicate_owner = len(row[0]) == 0
             if organization_is_not_approved or has_duplicate_owner:
                 continue
-            
+
             self.stdout.write(f"New organization: {row[0]}")
-            organization_name, _, _, owner_name, owner_email, _, _, _, time_and_place, social_links = row
+            (
+                organization_name,
+                _,
+                _,
+                owner_name,
+                owner_email,
+                _,
+                _,
+                _,
+                time_and_place,
+                social_links,
+            ) = row
 
             owner_user = None
             try:
@@ -61,7 +76,9 @@ class Command(BaseCommand):
                 )
 
                 skip_entry = False
-                self.stdout.write("\tIf you have the correct email, please enter it here:")
+                self.stdout.write(
+                    "\tIf you have the correct email, please enter it here:"
+                )
                 while True:
                     try:
                         print("\t", end="")
@@ -69,17 +86,23 @@ class Command(BaseCommand):
                         owner_user = User.objects.get(email=owner_email)
                         break
                     except User.DoesNotExist:
-                        if len(owner_email) == 0:
+                        if owner_email == "skip":
                             skip_entry = True
                             break
 
-                        self.stdout.write(self.style.ERROR("\tUser not found. Did you make a typo?"))
+                        self.stdout.write(
+                            self.style.ERROR(
+                                "\tUser not found. Did you make a typo? (type 'skip' to skip this entry)"
+                            )
+                        )
                         self.stdout.write("\tPlease re-enter email:")
 
-                if skip_entry == True:
+                if skip_entry:
                     self.stdout.write(f"\tSkipped creation of {organization_name}")
                     continue
-            self.stdout.write(self.style.SUCCESS(f"\tFound a match for {owner_name}'s email"))
+            self.stdout.write(
+                self.style.SUCCESS(f"\tFound a match for {owner_name}'s email")
+            )
 
             try:
                 # Consider using get_or_create() if it happens to be more useful?
@@ -89,9 +112,9 @@ class Command(BaseCommand):
                     name=organization_name,
                     bio="A WLMAC organization",
                     extra_content=time_and_place + "\n\n" + social_links,
-                    slug='' # very crude ik
-                        .join(c if c.isalnum() else '-' for c in organization_name)
-                        .lower(),
+                    slug="".join( 
+                        c.casefold() if c.isalnum() else "-" for c in organization_name
+                    ).lower(),
                     show_members=True,
                     is_active=True,
                     is_open=False,
@@ -100,7 +123,9 @@ class Command(BaseCommand):
                 club.execs.add(owner_user)
 
                 self.stdout.write(
-                    self.style.SUCCESS(f"\tSuccessfully added {organization_name} organization, owned by {owner_name}")
+                    self.style.SUCCESS(
+                        f"\tSuccessfully added {organization_name} organization, owned by {owner_name}"
+                    )
                 )
             except IntegrityError:
                 self.stdout.write(
