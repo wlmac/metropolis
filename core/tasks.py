78af978fe1c217d1dd25ddab7c6f4aacc1302a5d
pinwheeeel <phil.huang@student.tdsb.on.ca>
2025-10-14 20:21:41 -0400

    fix celery periodic tasks

--- core/tasks.py
+++ core/tasks.py
@@ -65,7 +65,7 @@ def users_with_token():
     return User.objects.exclude(Q(expo_notif_tokens=Value({}, JSONField())))
 
 
-@app.on_after_configure.connect
+@app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(crontab(hour=0, minute=0), delete_expired_users)
     sender.add_periodic_task(crontab(hour=18, minute=0), notif_events_singleday)
