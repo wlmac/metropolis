57ea0291df31a03577727a6963278f098b25d41a
imaegg11 <95944377+imaegg11@users.noreply.github.com>
2025-11-06 20:32:00 -0500

    switch to 2.5 flash

--- core/utils/ai.py
+++ core/utils/ai.py
@@ -2,12 +2,17 @@ from google import genai
 from django.conf import settings
 
 
-def prompt_gemini(prompt):
+def prompt_gemini(prompt, model=None):
     if settings.GEMINI_API_KEY is None:
         raise Exception("Gemini api key not found")
 
     client = genai.Client(api_key=settings.GEMINI_API_KEY)
-    model = settings.GEMINI_MODEL or "models/gemini-2.0-flash"
+
+    if model is None:
+        try:
+            model = settings.GEMINI_MODEL or "models/gemini-2.0-flash"
+        except AttributeError:
+            model = "models/gemini-2.0-flash"
 
     response = client.models.generate_content(
         model=model,
